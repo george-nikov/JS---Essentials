79490aa8dfd59a1e8d0051984ad2844fa7fa6c5e
Georgi Nikov <nikov.george@gmail.com>
2022-11-13 14:49:51 +0200

    Create grades.js

---
+++ Functions/grades.js
@@ -0,0 +1,21 @@
+function grades (input) {
+
+    let grade = Number(input[0]);
+
+    if (grade >= 2.00 && grade <= 2.99) {
+        console.log('Fail');
+    }
+    else if (grade <= 3.49) {
+        console.log('Poor');
+    }
+    else if (grade <= 4.49) {
+        console.log('Good');
+    }
+    else if (grade <= 5.49) {
+        console.log('Very good');
+    }
+    else if (grade <= 6.00) {
+        console.log('Excellent');
+    }
+}
+grades(['6.00'])
