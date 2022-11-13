2204057ea2642aa02c15cd4a5df550defeefe0c5
Georgi Nikov <nikov.george@gmail.com>
2022-11-13 14:47:23 +0200

    Create mathPower.js

---
+++ Functions/mathPower.js
@@ -0,0 +1,10 @@
+function mathPower (input) {
+
+    let number = Number(input[0]);
+    let index = Number(input[1]);
+
+    let result = Number(number ** index);
+
+    console.log(result);
+}
+mathPower([3,4])
