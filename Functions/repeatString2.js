f2c173b81abd27c87f49f49551d99a01e9c5dbfb
Georgi Nikov <nikov.george@gmail.com>
2022-11-13 14:49:04 +0200

    Create repeatString2.js

---
+++ Functions/repeatString2.js
@@ -0,0 +1,10 @@
+function repeatString (str, n) {
+
+    let result = '';
+
+    for (let index = 0; index < n; index++) {
+        result += str;
+    }
+    console.log(result);
+}
+repeatString(['String','2'])
